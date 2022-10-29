df19a066875fd57c7c2281e478a8141d09765856
ggomez0 <gaspargomez2000@outlook.com>
2022-10-29 05:01:30 -0300

    Reportes GoogleCharts

--- Admin/Reportes.aspx
+++ Admin/Reportes.aspx
@@ -1,3 +1,9 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Reportes.aspx.cs" Inherits="ShopGaspar.Admin.Reportes" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container">
+    <div id="chart_div" style="width:500px;height:400px">
+                <%-- Here Chart Will Load --%>
+            </div>
+        </div>
+        
 </asp:Content>
--- Admin/Reportes.aspx.cs
+++ Admin/Reportes.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Script.Services;
+using System.Web.Services;
+using ShopGaspar.Models;
 
 namespace ShopGaspar.Admin
 {
@@ -13,5 +16,33 @@ namespace ShopGaspar.Admin
         {
 
         }
+
+        //[WebMethod]
+        //[ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        //public static object[] GetChartData()
+        //{
+        //    List<Order> data = new List<Order>();
+
+        //    using (ProductContext dc = new ProductContext())
+        //    {
+        //        data = dc.Orders.ToList();
+        //    }
+
+        //    var chartData = new object[data.Count + 1];
+        //    chartData[0] = new object[]{
+        //        "Year",
+        //        "Electronics",
+        //        "Books & Media",
+        //        "Home & Kitchen"
+        //    };
+
+        //    int j = 0;
+        //    foreach (var i in data)
+        //    {
+        //        j++;
+        //        chartData[j] = new object[] { i.Address.ToString(), i.City, i.Country, i.Email };
+        //    }
+        //    return chartData;
+        //}
     }
 }
